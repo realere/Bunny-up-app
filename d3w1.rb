ef08c33afa3cf73363999b569e629cba526acea8
realere <prettyere@gmail.com>
2015-10-08 13:58:17 +0100

    Update d3w1.rb

--- d3w1.rb
+++ d3w1.rb
@@ -8,11 +8,5 @@ students.each_slice(how_big).to_a.each do |group|
 end
 end
 
-#a = [0, 1, 2, 3, 4, 5, 6, 7]
-#a.each_slice(3) # => #<Enumerator: [0, 1, 2, 3, 4, 5, 6, 7]:each_slice(3)>
-#a.each_slice(3).to_a # => [[0, 1, 2], [3, 4, 5], [6, 7]]
-#students.each_slice(3) # 
-#Prints students.each_slice(5).to_a
-
 puts "How big do you want the groups to be?"
 students(gets.chomp.to_i)
